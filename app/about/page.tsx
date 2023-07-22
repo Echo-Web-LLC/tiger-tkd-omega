62fc6c9537c3c39e45d51516049b9dad2e50bbe1
Sungmin Lee <sungminlee417@gmail.com>
2023-07-21 21:41:20 -0700

    added background info

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,4 +1,5 @@
 import AboutInfo from "@/components/about-components/AboutInfo";
+import BackgroundInfo from "@/components/about-components/BackgroundInfo";
 import InstructorInfo from "@/components/about-components/InstructorInfo";
 import PhotoGallery from "@/components/about-components/PhotoGallery";
 import React from "react";
@@ -7,6 +8,7 @@ const About = () => {
   return (
     <>
       <AboutInfo />
+      <BackgroundInfo />
       <InstructorInfo />
       <PhotoGallery />
     </>
---
+++ components/about-components/BackgroundInfo.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+
+const BackgroundInfo = () => {
+  return (
+    <section className="container mx-auto">
+      <div className="p-8 md:p-12 lg:px-16 lg:py-24 flex flex-col gap-8 justify-center text-center">
+        <h2 className="text-4xl font-semibold">A Piece of History</h2>
+        <div className="flex flex-col gap-4">
+          <p>
+            This school was founded by a very special Teacher. Grandmaster Seok
+            Joo "Tiger" Cheong was born in Korea and raised in the finest
+            institutions to learn the Martial Art of Taekwondo, widely regarded
+            as Korea's gift to the world. Grandmaster Tiger came to the states
+            back in the late 80's to start a school to allow anyone and everyone
+            to come in and better themselves. He had discovered his talent for
+            teaching after offering instruction in self defense to his workers
+            when he was a construction foreman in Kuwait back in the 70's. Some
+            Taekwondo historians argue that Grandmaster Tiger is the one
+            responsible for bringing Taekwondo to the Middle East! What started
+            as a way to protect the people in his charge would later reveal
+            itself to be his place in this world. In 1990, this school's doors
+            opened for the first time and have remained open to everyone ever
+            since.
+          </p>
+          <p>
+            We are dedicated to keeping the ideals of Grandmaster Tiger ever
+            present in our class. Grandmaster Tiger was a gentleman and a gentle
+            man; our teaching style will always embody this. No matter what
+            skill level our students may possess, we are here to believe in
+            every one of them.
+          </p>
+          <p>
+            While you are learning from us, we do our utmost to make sure you
+            are a gentle warrior in a beautiful garden of all your friends and
+            classmates; challenging yourself to grow and encouraging everyone
+            else to do the same. You will be shown incredible abilities which
+            will be used to protect yourself and everyone close to you. You will
+            look this power in the eye and bring it all under your control. You
+            will be shown all the nuances of your power so you may keep fear
+            from reaching those you care about.
+          </p>
+          <p>But knowing all of this is only half the battle.</p>
+          <p>
+            As disciples of Grandmaster Tiger, we will be here to guide every
+            one of our students to see the big picture; to help them see what is
+            good for them and everyone around them in the present moment and for
+            what is to come. Helping find one's place in the world is crucial at
+            any age. For youngsters, this helps to get them ready to seek out
+            who they are and what they want to be, giving them a jump-start on
+            their adult life; whenever maturity may reach them, they will be
+            ready. For those taking class with us later in their lives, we offer
+            a way to extend your well-being as far as we can take it, and even
+            offer a path to enlightenment should you come here seeking something
+            more for yourself.
+          </p>
+          <p>
+            At first it may seem that Martial Arts has no definitive function or
+            form, and this is what gives it so much meaning! Martial Arts can be
+            ANYTHING, so come on over and make it into whatever you want it to
+            be! Find your place here, and continue the story that Grandmaster
+            Tiger started over 30 years ago.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default BackgroundInfo;
